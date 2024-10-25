d96a27851147f2397786806115646ab6eee76717
kieuxuanloc3111 <chokeslam311@gmail.com>
2024-10-25 10:21:27 +0700

    process and transform

--- Faster R CNN/utils/transforms.py
+++ Faster R CNN/utils/transforms.py
@@ -27,7 +27,6 @@ def get_iou(boxes1, boxes2):
     return iou
 
 
-
 def boxes_to_transformation_targets(ground_truth_boxes, anchors_or_proposals):
     r"""
     Given all anchor boxes or proposals in image and their respective
@@ -58,6 +57,7 @@ def boxes_to_transformation_targets(ground_truth_boxes, anchors_or_proposals):
     targets_dh = torch.log(gt_heights / heights)
     regression_targets = torch.stack((targets_dx, targets_dy, targets_dw, targets_dh), dim=1)
     return regression_targets
+
 
 def apply_regression_pred_to_anchors_or_proposals(box_transform_pred, anchors_or_proposals):
     r"""
@@ -96,7 +96,7 @@ def apply_regression_pred_to_anchors_or_proposals(box_transform_pred, anchors_or
     pred_box_x1 = pred_center_x - 0.5 * pred_w
     pred_box_y1 = pred_center_y - 0.5 * pred_h
     pred_box_x2 = pred_center_x + 0.5 * pred_w
-    pred_box_y2 = pred_center_y + 0.5 * pred_h 
+    pred_box_y2 = pred_center_y + 0.5 * pred_h
     
     pred_boxes = torch.stack((
         pred_box_x1,
@@ -106,3 +106,67 @@ def apply_regression_pred_to_anchors_or_proposals(box_transform_pred, anchors_or
         dim=2)
     # pred_boxes -> (num_anchors_or_proposals, num_classes, 4)
     return pred_boxes
+
+
+def sample_positive_negative(labels, positive_count, total_count):
+    # Sample positive and negative proposals
+    positive = torch.where(labels >= 1)[0]
+    negative = torch.where(labels == 0)[0]
+    num_pos = positive_count
+    num_pos = min(positive.numel(), num_pos)
+    num_neg = total_count - num_pos
+    num_neg = min(negative.numel(), num_neg)
+    perm_positive_idxs = torch.randperm(positive.numel(),
+                                        device=positive.device)[:num_pos]
+    perm_negative_idxs = torch.randperm(negative.numel(),
+                                        device=negative.device)[:num_neg]
+    pos_idxs = positive[perm_positive_idxs]
+    neg_idxs = negative[perm_negative_idxs]
+    sampled_pos_idx_mask = torch.zeros_like(labels, dtype=torch.bool)
+    sampled_neg_idx_mask = torch.zeros_like(labels, dtype=torch.bool)
+    sampled_pos_idx_mask[pos_idxs] = True
+    sampled_neg_idx_mask[neg_idxs] = True
+    return sampled_neg_idx_mask, sampled_pos_idx_mask
+
+
+def clamp_boxes_to_image_boundary(boxes, image_shape):
+    boxes_x1 = boxes[..., 0]
+    boxes_y1 = boxes[..., 1]
+    boxes_x2 = boxes[..., 2]
+    boxes_y2 = boxes[..., 3]
+    height, width = image_shape[-2:]
+    boxes_x1 = boxes_x1.clamp(min=0, max=width)
+    boxes_x2 = boxes_x2.clamp(min=0, max=width)
+    boxes_y1 = boxes_y1.clamp(min=0, max=height)
+    boxes_y2 = boxes_y2.clamp(min=0, max=height)
+    boxes = torch.cat((
+        boxes_x1[..., None],
+        boxes_y1[..., None],
+        boxes_x2[..., None],
+        boxes_y2[..., None]),
+        dim=-1)
+    return boxes
+
+
+def transform_boxes_to_original_size(boxes, new_size, original_size):
+    r"""
+    Boxes are for resized image (min_size=600, max_size=1000).
+    This method converts the boxes to whatever dimensions
+    the image was before resizing
+    :param boxes:
+    :param new_size:
+    :param original_size:
+    :return:
+    """
+    ratios = [
+        torch.tensor(s_orig, dtype=torch.float32, device=boxes.device)
+        / torch.tensor(s, dtype=torch.float32, device=boxes.device)
+        for s, s_orig in zip(new_size, original_size)
+    ]
+    ratio_height, ratio_width = ratios
+    xmin, ymin, xmax, ymax = boxes.unbind(1)
+    xmin = xmin * ratio_width
+    xmax = xmax * ratio_width
+    ymin = ymin * ratio_height
+    ymax = ymax * ratio_height
+    return torch.stack((xmin, ymin, xmax, ymax), dim=1)
