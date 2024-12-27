17ba7434067de6fc6cf784a7e8010ee7ca015cd7
kieuxuanloc3111 <chokeslam311@gmail.com>
2024-12-27 14:49:33 +0700

    backbone

--- DETR/model/backbone.py
+++ DETR/model/backbone.py
@@ -3,7 +3,7 @@ from typing import List, Tuple
 import torch
 from torch import nn, Tensor
 
-from embedding import PositionEmbeddingSine
+from DETR.model.embedding import PositionEmbeddingSine
 
 
 class TransitionLayer(nn.Module):
